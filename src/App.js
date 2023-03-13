20e798b3602ad44e76b72b8251fb70096c4382cd
Fernando del Pino <fherdelpino@hotmail.com>
2023-03-14 00:36:45 +1100

    add call to fherdelpino.appspot.com api (#1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    fetch("http://fherdelpino.appspot.com/expense/all")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setExpenses(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Expenses</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>date</th>
+            <th>amount</th>
+            <th>description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr>
+              <td>{expense.name}</td>
+              <td>{expense.date}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }

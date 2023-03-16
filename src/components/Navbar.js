a5048ca93cb529b53d67e33548deacf6b149ab97
Fernando del Pino <fherdelpino@hotmail.com>
2023-03-17 00:26:56 +1100

    add bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,32 @@
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1>Expenses</h1>
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/create">New expense</Link>
-      </div>
-    </nav>
+    <div class="container">
+      <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a
+          href="/"
+          class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+        >
+          <svg class="bi me-2" width="40" height="32">
+            <use href="#bootstrap"></use>
+          </svg>
+          <span class="fs-4">Expenses</span>
+        </a>
+
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <Link to="/" className="nav-link active">
+              Home
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/create" className="nav-link">
+              New expense
+            </Link>
+          </li>
+        </ul>
+      </header>
+    </div>
   );
 };
 

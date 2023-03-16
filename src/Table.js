f0294b5e0d5d1108138e14dd869198ad3b58a442
Fernando del Pino <fherdelpino@hotmail.com>
2023-03-16 21:34:08 +1100

    fix commit bugs

--- src/Table.js
+++ src/Table.js
@@ -1,19 +1,29 @@
-import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 const Table = ({ title, columnHeaders, data }) => {
-  const history = useHistory();
+  const [records, setRecords] = useState(data);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   const handleDelete = (id) => {
-    console.log("Deleting..." + id);
     fetch("https://fherdelpino.appspot.com/expense/" + id, {
       method: "DELETE",
-    }).then(() => {
-      history.push("/");
-    });
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Couldn't delete the record.");
+        }
+        setErrorMsg(null);
+        const newRecords = records.filter((record) => record.id !== id);
+        setRecords(newRecords);
+      })
+      .catch((error) => {
+        setErrorMsg(error.message);
+      });
   };
   return (
     <div className="table">
       <h2>{title}</h2>
+      {errorMsg && <p>{errorMsg}</p>}
       <table>
         <thead>
           <tr>
@@ -23,7 +33,7 @@ const Table = ({ title, columnHeaders, data }) => {
           </tr>
         </thead>
         <tbody>
-          {data.map((record) => (
+          {records.map((record) => (
             <tr key={record.id}>
               <td>{record.name}</td>
               <td>{record.date}</td>
